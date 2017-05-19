be8c6bfc5d7f2b3585b1eb1133316afe46ba279b
Jerry Zang <jerry@ltc.tw>
2017-05-19 20:48:57 +0800

    17.05.19

--- client/main.ts
+++ client/main.ts
@@ -4,25 +4,30 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
+Template.hello.onCreated(function () {
+
 });
 
 Template.hello.helpers({
-    param: function() {
-        return FlowRouter.getParam('id');
-    },
-    name: "Jerry",
-    user: true,
-    tasks: [
-        { text: 'This is task 1' },
-        { text: 'This is task 2' },
-        { text: 'This is task 3' },
-    ],
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+  getNumberFromText: function (text: string,) {
+    return text.substr(13, 1);
+  },
+  fetchData: function() {
+    // const data = table.find({all});
+    return 'data'
+  }
 });
 
 Template.hello.events({
-  'click button'() {
-      FlowRouter.go('/info');
+  'mouseenter #red'(e) {
+    $(e.target).css("background-color", "red");
   },
+  'mouseleave #red'(e) {
+    $(e.target).css("background-color", "black");
+  }
 });
